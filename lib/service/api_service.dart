29a03d55b1ec4dec05784cc0364adef69fe9a91b
koldin108902 <alex108902@naver.com>
2023-03-02 05:54:28 +0900

    fixed: login session cookie 임시 저장

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -3,18 +3,35 @@ import 'dart:convert';
 import 'package:daejeon_fe/model/common/login_result_model.dart';
 import 'package:daejeon_fe/model/common/result_model.dart';
 import 'package:daejeon_fe/model/join_model.dart';
+import 'package:daejeon_fe/model/post/post_list_model.dart';
 import 'package:daejeon_fe/model/school_list_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String _domain = "http://172.30.1.51:80";
+  static Map<String, String> headers = {
+    "Content-Type": "application/json",
+    "withCredentials": "true"
+  };
+
+  static Future<PostListModel> getPostList({
+    required int page,
+  }) async {
+    var url = Uri.parse("$_domain/posts/?page=$page");
+
+    var res = await http.post(url, headers: headers);
+    if (res.statusCode != 200) throw Exception(res.statusCode);
+    var postList = PostListModel.fromJson(jsonDecode(res.body));
+
+    return postList;
+  }
 
   static writePost({required String description}) async {
     var url = Uri.parse("$_domain/post/write");
 
     var res = await http.post(
       url,
-      headers: {"Content-Type": "application/json"},
+      headers: headers,
       body: jsonEncode({'description': description}),
     );
 
@@ -27,11 +44,14 @@ class ApiService {
 
     var res = await http.post(
       url,
-      headers: <String, String>{
+      headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
+        "withCredentials": "true",
       },
       body: <String, String>{'loginId': id, 'password': password},
     );
+
+    _updateCookie(res);
 
     if (res.statusCode != 200) throw Error();
     var body = LoginResult.fromJson(jsonDecode(res.body));
@@ -44,9 +64,7 @@ class ApiService {
     var url = Uri.parse("$_domain/sign-up");
     var res = await http.post(
       url,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: headers,
       body: jsonEncode(body.toJson()),
     );
 
@@ -61,10 +79,19 @@ class ApiService {
     var res = await http.get(url);
 
     if (res.statusCode != 200) throw Exception(res.statusCode);
-    var result = Result.fromJson(jsonDecode(res.body));
+    var result = Result.fromJson(jsonDecode(utf8.decode(res.bodyBytes)));
     for (var school in result.data) {
       schoolList.add(SchoolListModel.fromJson(school));
     }
     return schoolList;
+  }
+
+  static void _updateCookie(http.Response response) {
+    String? rawCookie = response.headers['set-cookie'];
+    if (rawCookie != null) {
+      int index = rawCookie.indexOf(';');
+      headers['cookie'] =
+          (index == -1) ? rawCookie : rawCookie.substring(0, index);
+    }
   }
 }
