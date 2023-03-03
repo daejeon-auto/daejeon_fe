55289d168a934044e6d83802948ac3e3bca986ac
koldin108902 <alex108902@naver.com>
2023-03-03 17:20:05 +0900

    update: loading 이미지 및 주의 사항 추가

--- lib/screen/post_add_screen.dart
+++ lib/screen/post_add_screen.dart
@@ -1,6 +1,7 @@
 import 'package:daejeon_fe/screen/login_screen.dart';
 import 'package:daejeon_fe/service/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class PostAddScreen extends StatefulWidget {
   const PostAddScreen({Key? key}) : super(key: key);
@@ -11,6 +12,8 @@ class PostAddScreen extends StatefulWidget {
 
 class _PostAddScreenState extends State<PostAddScreen> {
   final descController = TextEditingController();
+  bool isLoading = false;
+  String errorMsg = "";
 
   @override
   void dispose() {
@@ -20,10 +23,18 @@ class _PostAddScreenState extends State<PostAddScreen> {
 
   addPost() async {
     try {
+      setState(() {
+        isLoading = true;
+      });
       await ApiService.writePost(description: descController.text);
       // ignore: use_build_context_synchronously
       return Navigator.pop(context);
     } on Exception catch (e) {
+      isLoading = false;
+      if (e.toString() == "Exception: 400") {
+        errorMsg = "글에 비속어가 포합돼있거나 15자 이내, 혹은 100자 이상입니다.";
+      }
+      setState(() {});
       if (e.toString() == "Exception: 401") {
         return Navigator.push(
           context,
@@ -32,6 +43,9 @@ class _PostAddScreenState extends State<PostAddScreen> {
           ),
         );
       }
+    } catch (e) {
+      errorMsg = e.toString();
+      setState(() {});
     }
   }
 
@@ -40,10 +54,24 @@ class _PostAddScreenState extends State<PostAddScreen> {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(15),
+        padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
         child: SingleChildScrollView(
           child: Column(
             children: [
+              if (errorMsg.isNotEmpty)
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  margin: const EdgeInsets.only(bottom: 10),
+                  color: Colors.red.withOpacity(0.5),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      errorMsg,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
               TextField(
                 controller: descController,
                 keyboardType: TextInputType.multiline,
@@ -59,13 +87,18 @@ class _PostAddScreenState extends State<PostAddScreen> {
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: addPost,
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color(0xFFE9EFFF),
-                ),
-                child: const Text("작성"),
-              ),
+              isLoading
+                  ? LoadingAnimationWidget.staggeredDotsWave(
+                      color: Colors.blueAccent,
+                      size: 50,
+                    )
+                  : TextButton(
+                      onPressed: addPost,
+                      style: TextButton.styleFrom(
+                        backgroundColor: const Color(0xFFE9EFFF),
+                      ),
+                      child: const Text("작성"),
+                    ),
               const SizedBox(
                 height: 40,
               ),
