828637511525b2098db4569d655d8473e502637c
koldin108902 <alex108902@naver.com>
2023-03-03 10:36:39 +0900

    update: ssl vertication disable

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:daejeon_fe/screen/join_screen.dart';
 import 'package:daejeon_fe/screen/login_screen.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'screen/home_screen.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(MaterialApp(
     routes: {
       "/login": (context) => const LoginScreen(),
@@ -12,4 +16,13 @@ void main() {
     },
     home: const App(),
   ));
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
